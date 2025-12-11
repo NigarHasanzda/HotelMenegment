7f5e2fb6450402d61d4a5e899c131336dae8da98
NigarHasanzda <nigarhesenzade703@gmail.com>
2025-12-11 19:03:09 +0400

    Main folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { createBrowserRouter, RouterProvider } from 'react-router'
 import './App.css'
+import Layout from './components/Layout/Layout'
+import Home from './components/Pages/Home/Home'
+import Login from './components/Login/Login'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        { path: "/", element: <Home /> },
+      ]
+    },
+    {
+      path:"/login",
+      element: <Login/>
+    }
+  ])
+
+  return <RouterProvider router={router} />
 }
 
 export default App
